074a1b4b986cee8afc2c4b68a0f160feab8e0a4e
Eduardo da Silva Araújo <eduardodevfull@gmail.com>
2021-10-05 20:53:32 -0300

    consertando bugs

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MainPageComponent } from '../main-page/main-page.component';
 import { OmbudsmanComponent } from './ombudsman/ombudsman.component';
 
 @NgModule({
