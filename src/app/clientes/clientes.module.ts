75d2a13eb1233d5fd8fab971dd9b41780024d2cf
Eduardo da Silva Araújo <eduardodevfull@gmail.com>
2021-10-05 14:02:57 -0300

    implementando a pagina de reclmacoes

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MainPageComponent } from '../main-page/main-page.component';
 import { OmbudsmanComponent } from './ombudsman/ombudsman.component';
 
 @NgModule({
@@ -9,7 +10,7 @@ import { OmbudsmanComponent } from './ombudsman/ombudsman.component';
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   exports: [
     OmbudsmanComponent
